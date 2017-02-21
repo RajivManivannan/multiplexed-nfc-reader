be48c84a49815660dbae80b61feb9cc9370b7cce
Gaurav Agarwal <gauravagarwalr@gmail.com>
2017-02-21 20:42:23 +0530

    Fixing code for reading and cleanup.

--- multiplexed_nfc_reader.py
+++ multiplexed_nfc_reader.py
@@ -18,29 +18,43 @@ class MultiplexedNFCReader:
         GPIO.setup(MultiplexedNFCReader.A2, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A1, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A0, GPIO.OUT)
-        self.mfrfcReader = MFRC522.MFRC522()
+        self.mfrfc_reader = MFRC522.MFRC522()
 
-    def selectDevice(self, deviceNumber):
-        self.deviceNumber = deviceNumber
+    def select_device(self, deviceNumber):
         GPIO.output(MultiplexedNFCReader.A2, (deviceNumber >> 2) & 1)
         GPIO.output(MultiplexedNFCReader.A1, (deviceNumber >> 1) & 1)
         GPIO.output(MultiplexedNFCReader.A0, deviceNumber & 1)
 
-    def readNFC(self):
-        (status, uid) = self.mfrfcReader.MFRC522_Anticoll()
+    def has_tag(self):
+        (status, TagType) = self.mfrfc_reader.MFRC522_Request(self.mfrfc_reader.PICC_REQIDL)
+        return status == self.mfrfc_reader.MI_OK
 
-        if status == self.mfrfcReader.MI_OK:
-          print "Card read " + str(self.deviceNumber) + "! UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+    def read_NFC(self):
+        (status, uid) = self.mfrfc_reader.MFRC522_Anticoll()
+        if status == self.mfrfc_reader.MI_OK:
+            return str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+        else:
+            return ""
 
     def cleanup(self):
         GPIO.cleanup()
 
+continue_reading = True
 
-multiplexedNFCReader = MultiplexedNFCReader()
+multiplexed_nfc_reader = MultiplexedNFCReader()
 
-while 1:
+def end_read(signal,frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    multiplexed_nfc_reader.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+while continue_reading:
     for device in range(0, 8):
-        multiplexedNFCReader.selectDevice(device)
-        multiplexedNFCReader.readNFC()
-
-multiplexedNFCReader.cleanup()
+        multiplexed_nfc_reader.select_device(device)
+        if multiplexed_nfc_reader.has_tag():
+            tag_uid = multiplexed_nfc_reader.read_NFC()
+            print "Card read " + str(device) + "! UID: "+ tag_uid
