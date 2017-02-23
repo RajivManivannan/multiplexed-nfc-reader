a12b83ee6279277a1bea97f1de07717c989f316f
Gaurav Agarwal <gauravagarwalr@gmail.com>
2017-02-23 16:45:33 +0530

    Changing config for 5 to 32 demux.

--- multiplexed_nfc_reader.py
+++ multiplexed_nfc_reader.py
@@ -8,10 +8,11 @@ import spidev
 import time
 
 class MultiplexedNFCReader:
-    A0 = 40 # GPIO 21
-    A1 = 38 # GPIO 20
-    A2 = 36 # GPIO 16
-    A3 = 32 # GPIO 12
+    A0 = 37 # GPIO 26
+    A1 = 35 # GPIO 19
+    A2 = 33 # GPIO 13
+    A3 = 31 # GPIO 6
+    A4 = 29 # GPIO 5
 
     def __init__(self, device_number):
         GPIO.setwarnings(False)
@@ -20,21 +21,24 @@ class MultiplexedNFCReader:
         GPIO.setup(MultiplexedNFCReader.A1, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A2, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A3, GPIO.OUT)
+        GPIO.setup(MultiplexedNFCReader.A4, GPIO.OUT)
         self.select_device(device_number)
         self.mfrfc_reader = MFRC522.MFRC522()
 
     def select_device(self, device_number):
+        a4_value = (device_number >> 4) & 1
         a3_value = (device_number >> 3) & 1
         a2_value = (device_number >> 2) & 1
         a1_value = (device_number >> 1) & 1
         a0_value = device_number & 1
 
-        print "Setting value: " + str(a3_value) + " " + str(a2_value) + " " + str(a1_value) + " " + str(a0_value)
+        print "Setting value: " + str(a4_value) + " " + str(a3_value) + " " + str(a2_value) + " " + str(a1_value) + " " + str(a0_value)
 
         GPIO.output(MultiplexedNFCReader.A0, a0_value)
         GPIO.output(MultiplexedNFCReader.A1, a1_value)
         GPIO.output(MultiplexedNFCReader.A2, a2_value)
         GPIO.output(MultiplexedNFCReader.A3, a3_value)
+        GPIO.output(MultiplexedNFCReader.A4, a4_value)
 
     def has_tag(self):
         (status, TagType) = self.mfrfc_reader.MFRC522_Request(self.mfrfc_reader.PICC_REQIDL)
@@ -60,7 +64,7 @@ def end_read(signal,frame):
 signal.signal(signal.SIGINT, end_read)
 
 while continue_reading:
-    for device in range(0, 16):
+    for device in range(0, 32):
         multiplexed_nfc_reader = MultiplexedNFCReader(device)
         print "Reading device: " + str(device)
         for _ in range(10):
