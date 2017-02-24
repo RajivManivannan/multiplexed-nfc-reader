f87627886b636e4a7b2368a2e61f65f1ed8b46a0
rajiv manivannan <rajiv@Rajivs-MacBook-Pro.local>
2017-02-24 17:08:16 +0530

    fixed error.

--- multiplexed_nfc_reader.py
+++ multiplexed_nfc_reader.py
@@ -53,7 +53,7 @@ class MultiplexedNFCReader:
             self.mfrfc_reader.MFRC522_SelectTag(uid)
             data = self.mfrfc_reader.MFRC522_DumpClassic1K(key, uid)
             self.mfrfc_reader.MFRC522_StopCrypto1()
-            if block not None:
+            if block is not None:
                 for block in data:
                     b = ""
                     for byte in block:
