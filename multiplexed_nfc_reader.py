14f388f1136d9b9191bf5e9bf286ed91eb5cd0fd
rajiv manivannan <rajiv@Rajivs-MacBook-Pro.local>
2017-02-24 15:43:33 +0530

    Reverted back to previous version and changed auth key.

--- multiplexed_nfc_reader.py
+++ multiplexed_nfc_reader.py
@@ -8,29 +8,37 @@ import spidev
 import time
 
 class MultiplexedNFCReader:
-    A0 = 18 # GPIO 24
-    A1 = 16 # GPIO 23
-    A2 = 12 # GPIO 18
+    A0 = 37 # GPIO 26
+    A1 = 35 # GPIO 19
+    A2 = 33 # GPIO 13
+    A3 = 31 # GPIO 6
+    A4 = 29 # GPIO 5
 
     def __init__(self, device_number):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(MultiplexedNFCReader.A2, GPIO.OUT)
-        GPIO.setup(MultiplexedNFCReader.A1, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A0, GPIO.OUT)
+        GPIO.setup(MultiplexedNFCReader.A1, GPIO.OUT)
+        GPIO.setup(MultiplexedNFCReader.A2, GPIO.OUT)
+        GPIO.setup(MultiplexedNFCReader.A3, GPIO.OUT)
+        GPIO.setup(MultiplexedNFCReader.A4, GPIO.OUT)
         self.select_device(device_number)
         self.mfrfc_reader = MFRC522.MFRC522()
 
     def select_device(self, device_number):
+        a4_value = (device_number >> 4) & 1
+        a3_value = (device_number >> 3) & 1
         a2_value = (device_number >> 2) & 1
         a1_value = (device_number >> 1) & 1
         a0_value = device_number & 1
 
-        print "Setting value: " + str(a2_value) + " " + str(a1_value) + " " + str(a0_value)
+        print "Setting value: " + str(a4_value) + " " + str(a3_value) + " " + str(a2_value) + " " + str(a1_value) + " " + str(a0_value)
 
-        GPIO.output(MultiplexedNFCReader.A2, a2_value)
-        GPIO.output(MultiplexedNFCReader.A1, a1_value)
         GPIO.output(MultiplexedNFCReader.A0, a0_value)
+        GPIO.output(MultiplexedNFCReader.A1, a1_value)
+        GPIO.output(MultiplexedNFCReader.A2, a2_value)
+        GPIO.output(MultiplexedNFCReader.A3, a3_value)
+        GPIO.output(MultiplexedNFCReader.A4, a4_value)
 
     def has_tag(self):
         (status, TagType) = self.mfrfc_reader.MFRC522_Request(self.mfrfc_reader.PICC_REQIDL)
@@ -38,22 +46,17 @@ class MultiplexedNFCReader:
 
     def read_NFC(self):
         (status, uid) = self.mfrfc_reader.MFRC522_Anticoll()
+        print "status : " + str(status) + " uid : "+ str(uid)
         if status == self.mfrfc_reader.MI_OK:
-            # This is the default key for authentication
+             # This is the default key for authentication
             key = [0x00,0x00,0x00,0x00,0x00,0x00]
-
-            # Select the scanned tag
-            self.mfrfc_reader.MFRC522_SelectTag(uid)
-
             # Authenticate
             status = self.mfrfc_reader.MFRC522_Auth(self.mfrfc_reader.PICC_AUTHENT1A, 8, key, uid)
-
-            # Check if authenticated
             if status == self.mfrfc_reader.MI_OK:
-                print "Auth Success"
+                self.mfrfc_reader.MFRC522_Read(i)
             else:
                 print "Auth Failure"
-
+            # self.mfrfc_reader.MFRC522_DumpClassic1K(key, uid)
             return str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
         else:
             return ""
@@ -71,12 +74,11 @@ def end_read(signal,frame):
 signal.signal(signal.SIGINT, end_read)
 
 while continue_reading:
-    for device in range(0, 8):
+    for device in range(0, 32):
         multiplexed_nfc_reader = MultiplexedNFCReader(device)
         print "Reading device: " + str(device)
         for _ in range(10):
             if multiplexed_nfc_reader.has_tag():
                 tag_uid = multiplexed_nfc_reader.read_NFC()
                 print "Card read " + str(device) + "! UID: "+ tag_uid
-            multiplexed_nfc_reader.cleanup()
-        time.sleep(1)
+        multiplexed_nfc_reader.cleanup()
