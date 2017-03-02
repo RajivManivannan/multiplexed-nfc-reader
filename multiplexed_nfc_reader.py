34efd27a83fbbc8a515bf4c367efd4dfddfbbc60
Gaurav Agarwal <gauravagarwalr@gmail.com>
2017-03-02 09:46:45 +0530

    Adding the A5 pin to count till 40.

--- multiplexed_nfc_reader.py
+++ multiplexed_nfc_reader.py
@@ -8,19 +8,13 @@ import time
 import signal
 import datetime
 
-nfc_uuid_to_tool_map = {
-    "136,4,170,99": "Tool A",
-    "136,4,80,109": "Tool B",
-    "136,4,78,109": "Tool C",
-    "136,4,60,110": "Tool D"
-}
-
 class MultiplexedNFCReader:
-    A0 = 37 # GPIO 26
-    A1 = 35 # GPIO 19
-    A2 = 33 # GPIO 13
-    A3 = 31 # GPIO 6
-    A4 = 29 # GPIO 5
+    A0 = 3 # GPIO 2
+    A1 = 5 # GPIO 3
+    A2 = 7 # GPIO 4
+    A3 = 11 # GPIO 17
+    A4 = 13 # GPIO 27
+    A5 = 15 # GPIO 22
 
     def __init__(self, device_number):
         GPIO.setwarnings(False)
@@ -30,23 +24,24 @@ class MultiplexedNFCReader:
         GPIO.setup(MultiplexedNFCReader.A2, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A3, GPIO.OUT)
         GPIO.setup(MultiplexedNFCReader.A4, GPIO.OUT)
+        GPIO.setup(MultiplexedNFCReader.A5, GPIO.OUT)
         self.select_device(device_number)
         self.mfrfc_reader = MFRC522.MFRC522()
 
     def select_device(self, device_number):
+        a5_value = (device_number >> 5) & 1
         a4_value = (device_number >> 4) & 1
         a3_value = (device_number >> 3) & 1
         a2_value = (device_number >> 2) & 1
         a1_value = (device_number >> 1) & 1
         a0_value = device_number & 1
 
-        # print "Setting value: " + str(a4_value) + " " + str(a3_value) + " " + str(a2_value) + " " + str(a1_value) + " " + str(a0_value)
-
         GPIO.output(MultiplexedNFCReader.A0, a0_value)
         GPIO.output(MultiplexedNFCReader.A1, a1_value)
         GPIO.output(MultiplexedNFCReader.A2, a2_value)
         GPIO.output(MultiplexedNFCReader.A3, a3_value)
         GPIO.output(MultiplexedNFCReader.A4, a4_value)
+        GPIO.output(MultiplexedNFCReader.A5, a5_value)
 
     def has_tag(self):
         (status, TagType) = self.mfrfc_reader.MFRC522_Request(self.mfrfc_reader.PICC_REQIDL)
@@ -71,26 +66,21 @@ def end_read(signal,frame):
     GPIO.cleanup()
 signal.signal(signal.SIGINT, end_read)
 
-tool_history = []
+def main():
+    while continue_reading:
+        tag_ids = []
 
-while continue_reading:
-    tools = []
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    for device in range(0, 32):
-        multiplexed_nfc_reader = MultiplexedNFCReader(device)
-        # print "Reading device: " + str(device)
-        for _ in range(5):
-            if multiplexed_nfc_reader.has_tag():
-                tag_uid = multiplexed_nfc_reader.read_NFC()
-                if tag_uid.count("") > 0:
-                    try:
-                        tool_id = nfc_uuid_to_tool_map[tag_uid]
-                        tools.append(tool_id)
-                    except KeyError:
-                # print "Card read " + str(device) + "! UID: "+ tag_uid
-        multiplexed_nfc_reader.cleanup()
-    tool_history.append({
-        'timestamp': timestamp,
-        'tools': tools
-    })
-    print "Tools found: " + ", ".join(map(str, set(tools)))
+        timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+        for device in range(0, 40):
+            multiplexed_nfc_reader = MultiplexedNFCReader(device)
+            for _ in range(5):
+                if multiplexed_nfc_reader.has_tag():
+                    tag_uid = multiplexed_nfc_reader.read_NFC()
+                    if tag_uid.count("") > 0:
+                        tag_ids.append(tag_uid)
+            multiplexed_nfc_reader.cleanup()
+        print "Tag ids found: " + ", ".join(map(str, set(tag_ids)))
+
+if __name__ == "__main__":
+    main()
