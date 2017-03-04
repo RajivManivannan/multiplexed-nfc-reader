bcdeba162b6689e28fe57332221ea39367dc0e01
Gaurav Agarwal <gauravagarwalr@gmail.com>
2017-03-04 12:18:42 +0530

    Changing the threolds for bluetooth readers.

--- bluetooth.rb
+++ bluetooth.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
 counter = 0
-ten_seconds = 10
-time = (Time.now + ten_seconds).to_i
+time_interval = 2 # seconds
+counter_threshold = 10 # counts
+time = (Time.now + time_interval).to_i
 
 uuid_to_machine_zone = {
   "74278BDA-B644-4520-8F0C-720EAF059935" => "Machine A Zone", # CC2541
@@ -23,22 +24,27 @@ ARGF.each_line do |e|
 
     counter = counter + 1
 
-    if Time.now.to_i > time || counter == 50
+    if Time.now.to_i > time || counter == counter_threshold
       counter = 0
-      time = (Time.now + ten_seconds).to_i
+      time = (Time.now + time_interval).to_i
 
       uuid_with_average_rssi = all_reads.reduce({}) do |memo, (group_uuid, reads)|
         memo[group_uuid] = reads[:rssi] / reads[:count]
         memo
       end
 
-      closest_uuid = uuid_with_average_rssi.sort_by do |uuid, avg_rssi|
-        avg_rssi
-      end.first.first
+      sorted_uuids = uuid_with_average_rssi.sort_by do |uuid, avg_rssi|
+        if uuid == "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6"
+          -avg_rssi + 20
+        end
+        -avg_rssi
+      end
+
+      closest_uuid = sorted_uuids.first
 
       all_reads = {}
 
-      puts "In zone: " + uuid_to_machine_zone[closest_uuid]
+      puts "In zone: #{uuid_to_machine_zone[closest_uuid.first]} with RSSI: #{closest_uuid.last}"
     end
   end
 end
