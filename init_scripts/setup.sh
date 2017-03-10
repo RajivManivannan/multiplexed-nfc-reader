f6a71afab8eb99e212724c262cb7c7f3f227bfa2
Gaurav Agarwal <gauravagarwalr@gmail.com>
2017-03-10 08:38:22 +0530

    Don't run the bluetooth script under systemd.

--- init_scripts/setup.sh
+++ init_scripts/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo cp ./etc/systemd/system/nearest-beacon.service /etc/systemd/system/nearest-beacon.service
-sudo systemctl enable nearest-beacon.service
-sudo systemctl start nearest-beacon.service
-sudo systemctl status nearest-beacon.service
+# sudo cp ./etc/systemd/system/nearest-beacon.service /etc/systemd/system/nearest-beacon.service
+# sudo systemctl enable nearest-beacon.service
+# sudo systemctl start nearest-beacon.service
+# sudo systemctl status nearest-beacon.service
 
 sudo cp ./etc/systemd/system/nfc-reader.service /etc/systemd/system/nfc-reader.service
 sudo systemctl enable nfc-reader.service
